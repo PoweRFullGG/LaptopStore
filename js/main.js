9a417bf6aa5f37f51675ded386a7de8b8c635b48
P0N41K <103584932+PoweRFullGG@users.noreply.github.com>
2024-11-03 21:07:47 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,57 @@
 //Переменные для отслеживания состояния кнопки
-var bTNDiscount1Bool = false
-var bTNDiscount2Bool = false
-var bTNDiscount3Bool = false
+
+if (localStorage.getItem("bTNDiscount1Bool") === null) {
+    localStorage.setItem("bTNDiscount1Bool", false);
+}
+
+if (localStorage.getItem("bTNDiscount2Bool") === null) {
+    localStorage.setItem("bTNDiscount2Bool", false);
+}
+
+if (localStorage.getItem("bTNDiscount3Bool") === null) {
+    localStorage.setItem("bTNDiscount3Bool", false);
+}
+
+var bTNDiscount1Bool = localStorage.getItem("bTNDiscount1Bool") === 'true';
+var bTNDiscount2Bool = localStorage.getItem("bTNDiscount2Bool") === 'true';
+var bTNDiscount3Bool = localStorage.getItem("bTNDiscount3Bool") === 'true';
+
+function updatePricesBasedOnLocalStorage() {
+    const textPriceBtn1 = document.getElementById("prc1");
+    const buttonDiscount1 = document.getElementById("bTNDiscount1");
+    const conTextNB1 = document.getElementById("conTextNB1");
+
+    if (bTNDiscount1Bool) {
+        OldPrice("₽49,990", conTextNB1, 1);
+        textPriceBtn1.innerText = "₽29,500";
+        textPriceBtn1.style.color = "red";
+        buttonDiscount1.innerText = "Купить по обычной цене";
+    }
+
+    const textPriceBtn2 = document.getElementById("prc2");
+    const buttonDiscount2 = document.getElementById("bTNDiscount2");
+    const conTextNB2 = document.getElementById("conTextNB2");
+
+    if (bTNDiscount2Bool) {
+        OldPrice("₽54,990", conTextNB2, 2);
+        textPriceBtn2.innerText = "₽45,500";
+        textPriceBtn2.style.color = "red";
+        buttonDiscount2.innerText = "Купить по обычной цене";
+    }
+
+    const textPriceBtn3 = document.getElementById("prc3");
+    const buttonDiscount3 = document.getElementById("bTNDiscount3");
+    const conTextNB3 = document.getElementById("conTextNB3");
+
+    if (bTNDiscount3Bool) {
+        OldPrice("₽129,990", conTextNB3, 3);
+        textPriceBtn3.innerText = "₽89,990";
+        textPriceBtn3.style.color = "red";
+        buttonDiscount3.innerText = "Купить по обычной цене";
+    }
+}
+
+updatePricesBasedOnLocalStorage();
 
 // Функция для создания старой цены с зачёркиванием
 function OldPrice(price, container, num) {
@@ -27,7 +77,9 @@ function OnClickButtonDiscount_1() {
         textPriceBtn1.innerText = "₽29,500";
         textPriceBtn1.style.color = "red";
         buttonDiscount1.innerText = "Купить по обычной цене";
-        bTNDiscount1Bool = true;
+
+        localStorage.setItem("bTNDiscount1Bool", true);
+        bTNDiscount1Bool = localStorage.getItem("bTNDiscount1Bool") === 'true';
     } else {
         const oldPrice = document.getElementById("oldPrice1");
         if (oldPrice) {
@@ -36,7 +88,9 @@ function OnClickButtonDiscount_1() {
         textPriceBtn1.innerText = "₽49,990";
         textPriceBtn1.style.color = "black";
         buttonDiscount1.innerText = "Купить со скидкой";
-        bTNDiscount1Bool = false;
+
+        localStorage.setItem("bTNDiscount1Bool", false);
+        bTNDiscount1Bool = localStorage.getItem("bTNDiscount1Bool") === 'true';
     }
 }
 
@@ -53,7 +107,9 @@ function OnClickButtonDiscount_2() {
         textPriceBtn2.innerText = "₽45,500";
         textPriceBtn2.style.color = "red";
         buttonDiscount2.innerText = "Купить по обычной цене";
-        bTNDiscount2Bool = true;
+
+        localStorage.setItem("bTNDiscount2Bool", true);
+        bTNDiscount2Bool = localStorage.getItem("bTNDiscount2Bool") === 'true';
     } else {
         const oldPrice = document.getElementById("oldPrice2");
         if (oldPrice) {
@@ -62,7 +118,9 @@ function OnClickButtonDiscount_2() {
         textPriceBtn2.innerText = "₽54,990";
         textPriceBtn2.style.color = "black";
         buttonDiscount2.innerText = "Купить со скидкой";
-        bTNDiscount2Bool = false;
+
+        localStorage.setItem("bTNDiscount2Bool", false);
+        bTNDiscount2Bool = localStorage.getItem("bTNDiscount2Bool") === 'true';
     }
 }
 
@@ -79,7 +137,9 @@ function OnClickButtonDiscount_3() {
         textPriceBtn3.innerText = "₽89,990";
         textPriceBtn3.style.color = "red";
         buttonDiscount3.innerText = "Купить по обычной цене";
-        bTNDiscount3Bool = true;
+
+        localStorage.setItem("bTNDiscount3Bool", true);
+        bTNDiscount3Bool = localStorage.getItem("bTNDiscount3Bool") === 'true';
     } else {
         const oldPrice = document.getElementById("oldPrice3");
         if (oldPrice) {
@@ -88,7 +148,9 @@ function OnClickButtonDiscount_3() {
         textPriceBtn3.innerText = "₽129,990";
         textPriceBtn3.style.color = "black";
         buttonDiscount3.innerText = "Купить со скидкой";
-        bTNDiscount3Bool = false;
+
+        localStorage.setItem("bTNDiscount3Bool", false);
+        bTNDiscount3Bool = localStorage.getItem("bTNDiscount3Bool") === 'true';
     }
 }
 
